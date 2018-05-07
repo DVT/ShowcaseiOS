2d8a53ba118dbed61f8725004ca601156f78d129
Kagiso <Kmohajane@jhb.dvt.co.za>
2018-05-07 14:15:34 +0200

    Correct usage of optional

--- Showcase-iOS/Contact Us/Model/Office.swift
+++ Showcase-iOS/Contact Us/Model/Office.swift
@@ -15,16 +15,14 @@ class Office {
     var telephone: String?
     
     init(with snapShotValue: [String: Any]?) {
-        if let snapShotValue = snapShotValue {
-        self.address = snapShotValue["address"] as? String
-        self.emailAddress = snapShotValue["emailAddress"] as? String
-        self.googleMapsName = snapShotValue["googleMapsName"] as? String
-        self.googleMapsPlaceId = snapShotValue["googleMapsPlaceId"] as? String
-        self.image = snapShotValue["image"] as? String
-        self.latitude = snapShotValue["latitude"] as? String
-        self.longitude = snapShotValue["longitude"] as? String
-        self.name = snapShotValue["name"] as? String
-        self.telephone = snapShotValue["telephone"] as? String
-        }
+        self.address = snapShotValue?["address"] as? String
+        self.emailAddress = snapShotValue?["emailAddress"] as? String
+        self.googleMapsName = snapShotValue?["googleMapsName"] as? String
+        self.googleMapsPlaceId = snapShotValue?["googleMapsPlaceId"] as? String
+        self.image = snapShotValue?["image"] as? String
+        self.latitude = snapShotValue?["latitude"] as? String
+        self.longitude = snapShotValue?["longitude"] as? String
+        self.name = snapShotValue?["name"] as? String
+        self.telephone = snapShotValue?["telephone"] as? String
     }
 }
