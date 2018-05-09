8de2c1008c51618129e5ec51d6786390403d64b1
Kagiso <Kmohajane@jhb.dvt.co.za>
2018-05-09 16:27:37 +0200

    Adding ContactUsCollectionViewCell,adding Popping font

---
+++ Showcase-iOS/Contact Us/View/ContactUsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+
+
+import UIKit
+
+class ContactUsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
