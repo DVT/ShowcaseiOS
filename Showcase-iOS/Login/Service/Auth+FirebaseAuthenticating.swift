b0d53428494d1c8c844dfeb1f4204b2e14eb4c29
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2018-05-16 15:00:29 +0200

    Cast signIn completion to be an AuthDataResultCallback

--- Showcase-iOS/Login/Service/Auth+FirebaseAuthenticating.swift
+++ Showcase-iOS/Login/Service/Auth+FirebaseAuthenticating.swift
@@ -11,6 +11,7 @@ import FirebaseAuth
 
 extension Auth: FirebaseLoginAuthenticating {
     func signIn(withEmail email: String, password: String, completion: @escaping (AuthDataResultProtocol?, Error?) -> Void) {
+        let completion = completion as AuthDataResultCallback
         self.signIn(withEmail: email, password: password, completion: completion)
     }
 }
