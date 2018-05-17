ee2e51694c5fcdf15b338fb3dbb07f392cfd70a7
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2018-05-17 12:58:36 +0200

    Feature/about ui (#27)
    * About screen UI
    
    * Update the padding for the images

---
+++ Showcase-iOS/About/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  Showcase-iOS
+//
+//  Created by Lehlohonolo Mbele on 2018/05/14.
+//  Copyright © 2018 DVT. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
