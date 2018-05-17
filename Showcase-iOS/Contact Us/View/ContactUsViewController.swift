9cb9d56a45bd1da27ec3e84005b15ce119c48ccd
Kagiso <Kmohajane@jhb.dvt.co.za>
2018-05-17 22:00:16 +0200

    Feature/contat us view controller (#35)
    * Adding ContactUsCollectionViewCell,adding Popping font
    
    * "Adding contactUS collectionView cell, collectionViewCell viewModel, contactUsDelegate protocol.
    
    * Adding contactUsCollectionViewCell and swift class.
    
    * Adding computed properties and shared applicationDelegate property. Removing map related code. Adding ContactUsCellViewModelTests.
    
    * Removing navigate method. Adding sharedApplicationDelegate and implementation.
    
    * Adding KingFischer Pod.
    
    * Updating run script
    
    * Adding ContactUsViewController with collectionView Extension and PresenterViewable extension.
    
    * Adding retrieve contacts method
    
    * Adding collectionView to storyboard
    
    * adding initial view controller property
    
    * Updating populateStaticMap method
    
    * Finalising Contact Us CollectionViewCell
    
    * adding collection view and delegate methods. Setting delegate and datasource. Adding mock data
    
    * Adding prefixes to email and call functionality for shred application
    
    * Embedding view controller in navigation controller
    
    * Adding plist
    
    * Adding project file

---
+++ Showcase-iOS/Contact Us/View/ContactUsViewController.swift
@@ -0,0 +1,99 @@
+
+
+import Foundation
+import UIKit
+
+class ContactUsViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    var contactUsPresenter: ContactUsPresenter?
+    var cellViewModel: ContactUsCellViewModel?
+    var officeViewModels = [OfficeViewModel]()
+    
+    //MARK: @IBOutlets
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    //MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.topItem?.title = "Contact Us"
+        setCollectionView()
+        registerContactUsCell()
+        contactUsPresenter?.retrieveContacts()
+    }
+    
+    //MARK: Operations
+    
+    func setCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    func registerContactUsCell() {
+        let contactUsNib  = UINib(nibName: ContactUsCollectionViewCell.identifier, bundle: nil)
+        collectionView.register(contactUsNib, forCellWithReuseIdentifier: ContactUsCollectionViewCell.identifier)
+    }
+    
+    //MARK: MockData
+    
+    func mockValidOfficeResponse() -> OfficeViewModel {
+        let mockJhbOffice: [String: Any] = ["latitude":"-26.1159126", "name":"Johannesburg","image":"offices/dvt_hyde_park.png",
+                                            "googleMapsPlaceId":"ChIJF0f-kTdzlR4RioXEaM2-a10",
+                                            "address":"Ground Floor,Victoria Gate South,Hyde Lane Office Park,Hyde Park Lane,Hydepark,Johannesburg,2196",
+                                            "googleMapsName":"DVT Johannesburg",
+                                            "emailAddress":"jvandermerwe@jhb.dvt.co.za",
+                                            "longitude":"28.0328262",
+                                            "telephone":"+2773444000"]
+        return OfficeViewModel(with: Office(with: mockJhbOffice))
+    }
+}
+
+//MARK: CollectionView extension
+
+extension ContactUsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 236)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section:Int) -> CGFloat {
+        return 2.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.officeViewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ContactUsCollectionViewCell.identifier, for: indexPath) as! ContactUsCollectionViewCell
+        cellViewModel = ContactUsCellViewModel(with: officeViewModels[indexPath.row])
+        cell.viewModel = cellViewModel
+        cell.viewModel?.sharedApplication = SharedApplicationDelegateImplementation()
+        cell.populateView()
+        return cell
+        
+    }
+}
+
+//MARK: Extension Presentable
+
+extension ContactUsViewController: ContactUsPresenterViewable {
+    func showOnSuccess(with officeViewModels: [OfficeViewModel]) {
+        self.officeViewModels = officeViewModels
+        collectionView.reloadData()
+    }
+    
+    func showOnFailure(with error: Error) {
+        //TO DO: Present error view
+    }
+    
+    
+}
+
