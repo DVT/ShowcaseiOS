bacf01e07043c10089c6115df819ed770864b7ba
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2018-05-23 21:12:59 +0200

    Feature/authenticate already logged in user (#43)
    * Authenticate users who are already logged in the app.
    
    * Abstract UserDefaults into a Protocol and write tests for it.
    
    * Fix a failing test.

---
+++ Showcase-iOS/Contact Us/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Showcase-iOS
+//
+//  Created by Lehlohonolo Mbele on 2018/05/22.
+//  Copyright © 2018 DVT. All rights reserved.
+//
+
+import Foundation
+
+enum UserDefaultsKeys: String {
+    case isLoggedIn
+}
--- Showcase-iOS/Login/Presenter/LoginPresentable.swift
+++ Showcase-iOS/Login/Presenter/LoginPresentable.swift
@@ -11,5 +11,7 @@ import Foundation
 protocol LoginPresentable {
     var loginViewer: LoginPresenterViewable? { get set}
     var loginInteractor: LoginPresenterInteractable? { get set}
+    var userDefaults: UserDefaultsProtocol? { get set}
+    func showSuccesWhenUserIsAlreadyAuthenticated()
     func login(withEmail email: String, password: String)
 }
