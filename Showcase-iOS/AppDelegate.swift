571000833b9c11f89f92ec318a4695f95c2cb5f4
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2018-05-23 12:59:56 +0200

    Replacy custom kekboard handling by using IQKeyboardManagerSwift

--- Showcase-iOS/AppDelegate.swift
+++ Showcase-iOS/AppDelegate.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        IQKeyboardManager.sharedManager().enable = true
         return true
     }
 
