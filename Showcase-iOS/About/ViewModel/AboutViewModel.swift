35c98cfbd6d3bc8a2b365118e560bc03613d767c
Sashen Pillay <sashenpillay943@gmail.com>
2019-03-20 13:19:53 +0200

    Folder naming fixed

--- Showcase-iOS/About/ViewModel/AboutViewModel.swift
+++ Showcase-iOS/About/ViewModel/AboutViewModel.swift
@@ -12,24 +12,24 @@ class AboutViewModel {
 
     var socialMediaLinks: SocialMediaLinks?
 
-    var twitter: URL {
+    var twitter: URL? {
         let url = URL(string: socialMediaLinks?.twitter ?? "https://twitter.com/dvt_corporate")
-        return url!
+        return url ?? nil
     }
 
-    var facebook: URL {
+    var facebook: URL? {
         let url = URL(string: socialMediaLinks?.facebook ?? "https://www.facebook.com/DVTSoftware")
-        return url!
+        return url ?? nil
     }
 
-    var instagram: URL {
+    var instagram: URL? {
         let url = URL(string: socialMediaLinks?.instagram ?? "https://www.instagram.com/dvtsoftware/")
-       return url!
+       return url ?? nil
     }
 
-    var website: URL {
+    var website: URL? {
         let url = URL(string: socialMediaLinks?.website ?? "https://www.dvt.co.za")
-        return url!
+        return url ?? nil
     }
 
     init(socialMediaLinks: SocialMediaLinks) {
