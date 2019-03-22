3968f0895218f4e3c45e10eded092921a3fb7a2a
Sashen Pillay <sashenpillay943@gmail.com>
2019-03-22 09:46:25 +0200

    Firebase - code clean up
    - Removed trailing white spaces
    - Added MARK comments
    - Removed author tags in comments

--- Showcase-iOS/Firebase/ImageFetcher/FIRImageFetcher.swift
+++ Showcase-iOS/Firebase/ImageFetcher/FIRImageFetcher.swift
@@ -4,13 +4,13 @@ import Foundation
 import Foundation
 
 class ImageFetcher {
-    
+
     private var imageReference: StorageReferenceable?
-    
+
      init(from storageService: FIRStoring) {
         imageReference = storageService.storageReference()
     }
-    
+
     func fetchImage(_ image: String,_ completed: @escaping (URL?, Error?) -> Void) {
         imageReference?.child(from: image).downloadImageUrl(completion: { imageUrl, error in
             if let error = error {
@@ -19,6 +19,6 @@ class ImageFetcher {
                 completed(imageUrl,nil)
             }
         })
-       
+
     }
 }
--- Showcase-iOS/Viewable.swift
+++ Showcase-iOS/Viewable.swift
@@ -1,11 +1,3 @@
-//
-//  Viewable.swift
-//  Showcase-iOS
-//
-//  Created by pjapple on 2018/04/17.
-//  Copyright © 2018 DVT. All rights reserved.
-//
-
 import Foundation
 protocol Viewable {
 }
