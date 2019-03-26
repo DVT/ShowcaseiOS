ce3f6daa2394a6ec629bc762eaca1db5983001c0
Sashen Pillay <sashenpillay943@gmail.com>
2019-03-26 09:46:04 +0200

    Removed trailing white spaces

--- Showcase-iOS/Firebase/Analytics/AnalyticManagerImplementation.swift
+++ Showcase-iOS/Firebase/Analytics/AnalyticManagerImplementation.swift
@@ -7,20 +7,20 @@ class AnalyticManagerImplementation: AnalyticsManager {
         let analyticTag = "button_tap_\(buttonName)"
         Analytics.logEvent(analyticTag, parameters: nil)
     }
-    
+
     func trackSelectionOfApplication(applicationName: String) {
         let analyticTag = "app_selected_\(applicationName)"
         Analytics.logEvent(analyticTag, parameters: nil)
     }
-    
+
     func trackScreenAppear(screenName: String) {
         let analyticTag = "screen_appear_\(screenName)"
         Analytics.logEvent(analyticTag, parameters: nil)
     }
-    
+
     func trackGenericEvent(eventName: String) {
         let analyticTag = "sgeneric_\(eventName)"
         Analytics.logEvent(analyticTag, parameters: nil)
     }
-    
+
 }
