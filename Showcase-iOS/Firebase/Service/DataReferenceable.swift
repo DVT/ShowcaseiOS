c3130def77fd85ed3f16e85374c6314611d71651
Sashen Pillay <sashenpillay943@gmail.com>
2019-04-04 14:18:09 +0200

    Code clean up of firebase files

--- Showcase-iOS/Firebase/Service/DataReferenceable.swift
+++ Showcase-iOS/Firebase/Service/DataReferenceable.swift
@@ -1,4 +1,3 @@
-
 import Firebase
 import FirebaseDatabase
 import Foundation
@@ -7,8 +6,10 @@ protocol DataReferenceable {
 
     func databaseReference() -> DataReferenceable?
     func child(_ path: Path) -> DataReferenceable?
-    func observe(eventType: DataEventType, with snapshot: @escaping (DataSnapshotProtocol) -> Void, withCancel cancel: @escaping (Error) -> Void)
-
+    func observe(eventType: DataEventType,
+                 with snapshot: @escaping (DataSnapshotProtocol) -> Void,
+                 withCancel cancel: @escaping (Error) -> Void)
+    
 }
 
 
--- Showcase-iOS/Firebase/Storage/StorageReferenceExtension.swift
+++ Showcase-iOS/Firebase/Storage/StorageReferenceExtension.swift
@@ -1,16 +1,15 @@
-
 import FirebaseStorage
 import Foundation
 
-
 extension StorageReference: StorageReferenceable {
+
     func child(from path: String) -> StorageReferenceable {
        return self.child(path)
     }
-    
+
     func downloadImageUrl(completion: @escaping (URL?, Error?) -> Void) {
         self.downloadURL(completion: completion)
     }
-    
+
 }
 
