cfcf3803d22aad51ddd000907fd651b1cc61cc29
Sashen Pillay <sashenpillay943@gmail.com>
2019-04-04 15:45:58 +0200

    Dashboard - clean up (#100)
    * Code clean up of dashboard
    
    * Codacy Comments fixed

--- Showcase-iOS/Dashboard/Presenter/HomePresentable.swift
+++ Showcase-iOS/Dashboard/Presenter/HomePresentable.swift
@@ -1,11 +1,3 @@
-//
-//  HomePresentable.swift
-//  Showcase-iOS
-//
-//  Created by Edward Mtshweni on 2018/05/02.
-//  Copyright © 2018 DVT. All rights reserved.
-//
-
 import Foundation
 
 protocol HomePresentable {
