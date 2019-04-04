297d96077c7ead9567945527f12ad1d65881c2e2
Sashen Pillay <sashenpillay943@gmail.com>
2019-04-04 14:36:14 +0200

    Codacy

--- Showcase-iOS/Firebase/Service/DataReferenceable.swift
+++ Showcase-iOS/Firebase/Service/DataReferenceable.swift
@@ -3,14 +3,11 @@ import FirebaseDatabase
 import Foundation
 
 protocol DataReferenceable {
-
     func databaseReference() -> DataReferenceable?
     func child(_ path: Path) -> DataReferenceable?
     func observe(eventType: DataEventType,
                  with snapshot: @escaping (DataSnapshotProtocol) -> Void,
                  withCancel cancel: @escaping (Error) -> Void)
-    
 }
-
 
 
